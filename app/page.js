1560dd9d35e4638e04465966b24b3783313b0b50
decadeus <debeaumont@decadeus.com>
2024-05-16 14:04:34 +0200

    bnbn

--- app/page.js
+++ app/page.js
@@ -142,3 +142,10 @@ export default function Home() {
     </main>
   );
 }
+
+export const config = {
+ env: {
+  NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+   NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+ }
+}
